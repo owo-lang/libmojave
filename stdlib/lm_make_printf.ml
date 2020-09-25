fd35b27d2c1943ffa25779673c4e68c29040663d
LdBeth <andpuke@foxmail.com>
2020-09-25 10:36:03 +0800

    Use GADT implementation of format spec
    instead of the old Obj.magic trick.

--- stdlib/lm_make_printf.ml
+++ stdlib/lm_make_printf.ml
@@ -42,7 +42,7 @@
  *    @<n>: print_length
  *    @@: plain @ char
  *
- * Note the copious use of Obj.magic
+ * This module now relies on CamlinternalFormat
  *
  * ----------------------------------------------------------------
  *
@@ -129,361 +129,67 @@ struct
    type t = Args.t
    type result = Args.result
 
-   (*
-    * Field flags.
-    *)
-   type format_flag =
-      AlternateForm
-    | ZeroPad
-    | LeftAdjust
-    | LeaveBlank
-    | AlwaysSign
+   open CamlinternalFormatBasics
+   open CamlinternalFormat
 
-   (*
-    * Types of int printing.
-    *)
-   type int_spec =
-      UnsignedOctal
-    | SignedDecimal
-    | UnsignedDecimal
-    | UnsignedHexLowercase
-    | UnsignedHexUppercase
-
-   (*
-    * Types of float printing.
-    *)
-   type float_spec =
-      FloatNormal
-    | FloatExp
-    | FloatBest
-
-   (*
-    * Field specifiers.
-    *)
-   type field_info =
-      { field_flags : format_flag list;
-        field_width : int option;
-        field_precision : int option
-      }
 
    (************************************************************************
     * BASE PRINTERS
     ************************************************************************)
 
-   (*
-    * Most of these basic printers just pass out the print
-    * request to C functions.  The first string is the format
-    * string to be passed to printf.
-    *)
-   external ext_print_char : string -> char -> string     = "ml_print_char"
-   external ext_print_int : string -> int -> string       = "ml_print_int"
-   external ext_print_float : string -> float -> string   = "ml_print_float"
-   external ext_print_string : string -> string -> string = "ml_print_string"
-   external ext_print_string2 : int -> string -> string -> string = "ml_print_string2"
+   let compute_tag acc =
+      let buf = Buffer.create 16 in
+      let rec p_acc b a = match a with
+         Acc_string_literal (p, s) -> p_acc b a; Buffer.add_string b s
+       | End_of_acc -> ()
+       | _ -> raise (Invalid_argument "Invalid print box tag")
+      in p_acc buf acc;
+      let len = Buffer.length buf in
+      if len < 2 then Buffer.contents buf
+      else Buffer.sub buf 1 (len - 2)
 
-   (*
-    * Maximum width specified in the format.
-    *)
-   let max_width info =
-      match info with
-         { field_width = Some width; field_precision = Some pre; _ } ->
-            max width pre
-       | { field_width = Some width; field_precision = None; _ }
-       | { field_width = None; field_precision = Some width; _ } ->
-            width
-       | { field_width = None; field_precision = None; _ } ->
-            0
+   let print_open_box buf indent = function
+      Pp_hbox -> Args.open_hbox buf
+    | Pp_vbox -> Args.open_vbox buf indent
+    | Pp_hvbox -> Args.open_hvbox buf indent
+    | Pp_hovbox -> Args.open_hovbox buf indent
+    | Pp_box -> Args.open_box buf indent
+    | Pp_fits -> raise (Invalid_argument "Unsupported fits type")
 
-   (*
-    * Next arg should be an int.
-    *)
-   let rec print_bool buf i len s fmt _info =
-      let print b =
-         let str = ext_print_string fmt (if b then "true" else "false") in
-            Args.print_string buf str;
-            print_loop buf i len s
-      in
-         Obj.magic print
+   let print_formatting_lit o fmting_lit = match fmting_lit with
+      Close_box                 -> Args.close_box o
+    | Close_tag                 -> ()
+    | Break ("@,", _, _)        -> Args.print_cut o
+    | Break ("@ ", _, _)        -> Args.print_space o
+    | Break (_, width, offset)  -> Args.print_break o width offset
+    | FFlush                    -> Args.print_flush o
+    | Force_newline             -> Args.force_newline o
+    | Flush_newline             -> Args.print_newline o
+    | Magic_size (_, _)         -> ()
+    | Escaped_at                -> Args.print_char o '@'
+    | Escaped_percent           -> Args.print_char o '%'
+    | Scan_indic c              -> Args.print_char o '@'; Args.print_char o c
 
-   and print_char buf i len s fmt _info =
-      let print c =
-         let str = ext_print_char fmt c in
-            Args.print_string buf str;
-            print_loop buf i len s
-      in
-         Obj.magic print
+   let rec print_acc o acc = match acc with
+      Acc_formatting_lit (p, f) ->
+         print_acc o p; print_formatting_lit o f;
+    | Acc_formatting_gen (p, Acc_open_tag acc') ->
+         print_acc o p; Args.print_string o "@{"; print_acc o acc'; (* not implemented *)
+    | Acc_formatting_gen (p, Acc_open_box acc') ->
+         print_acc o p;
+         let (indent, bty) = open_box_of_string (compute_tag acc') in
+         print_open_box o indent bty;
+    | Acc_string_literal (p, s)
+    | Acc_data_string (p, s)   -> print_acc o p; Args.print_string o s
+    | Acc_char_literal (p, c)
+    | Acc_data_char (p, c)     -> print_acc o p; Args.print_char o c
+    | Acc_delay (p, f)         -> print_acc o p; ignore (f o);
+    | Acc_flush p              -> print_acc o p; Args.print_flush o
+    | Acc_invalid_arg (p, msg) -> print_acc o p; raise (Invalid_argument msg);
+    | End_of_acc               -> ()
 
-   and print_int buf i len s fmt _info _spec =
-      let print x =
-         let str = ext_print_int fmt x in
-            Args.print_string buf str;
-            print_loop buf i len s
-      in
-         Obj.magic print
-
-   and print_float buf i len s fmt _info _spec =
-      let print x =
-         let str = ext_print_float fmt x in
-            Args.print_string buf str;
-            print_loop buf i len s
-      in
-         Obj.magic print
-
-   and print_string buf i len s fmt info =
-      let print str =
-         let str = ext_print_string2 (max_width info) fmt str in
-            Args.print_string buf str;
-            print_loop buf i len s
-      in
-         Obj.magic print
-
-   and print_user1 buf i len s _fmt _info =
-      let print f =
-         f buf;
-         print_loop buf i len s
-      in
-         Obj.magic print
-
-   and print_user2 buf i len s _fmt _info =
-      let print f x =
-         f buf x;
-         print_loop buf i len s
-      in
-         Obj.magic print
-
-   and print_dummy _buf _i _len _s =
-      Obj.magic ()
-
-   and print_percent buf i len s fmt _info =
-      let str = ext_print_string fmt "%" in
-         Args.print_string buf str;
-         print_loop buf i len s
-
-   (*
-    * Parse the format specification.
-    *)
-   and print_format buf index len s =
-      (*
-       * Read off any flag characters.
-       *)
-      let rec parse_flags flags i =
-         if i = len then
-            print_dummy buf i len s
-         else
-            let c = s.[i] in
-               match c with
-                  '#' ->
-                     parse_flags (AlternateForm :: flags) (succ i)
-                | '0' ->
-                     parse_flags (ZeroPad :: flags) (succ i)
-                | '-' ->
-                     parse_flags (LeftAdjust :: flags) (succ i)
-                | ' ' ->
-                     parse_flags (LeaveBlank :: flags) (succ i)
-                | '+' ->
-                     parse_flags (AlwaysSign :: flags) (succ i)
-                | '1'..'9' ->
-                     parse_field_width flags 0 i
-                | '.' ->
-                     parse_precision flags None 0 (succ i)
-                | _ ->
-                     parse_spec flags None None i
-
-      (*
-       * Read off the field width.
-       *)
-      and parse_field_width flags width i =
-         if i = len then
-            print_dummy buf i len s
-         else
-            let c = s.[i] in
-               match c with
-                  '0'..'9' ->
-                     parse_field_width flags (width * 10 + Char.code c - Char.code '0') (succ i)
-                | '.' ->
-                     parse_precision flags (Some width) 0 (succ i)
-                | _ ->
-                     parse_spec flags (Some width) None i
-
-      (*
-       * Parse the precision specifier.
-       *)
-      and parse_precision flags width pre i =
-         if i = len then
-            print_dummy buf i len s
-         else
-            let c = s.[i] in
-               match c with
-                  '0'..'9' ->
-                     parse_precision flags width (pre * 10 + Char.code c - Char.code '0') (succ i)
-                | _ ->
-                     parse_spec flags width (Some pre) i
-
-      (*
-       * Finally we have the format specifier.
-       *)
-      and parse_spec flags width pre i =
-         if i = len then
-            print_dummy buf i len s
-         else
-            let info =
-               { field_flags = List.rev flags;
-                 field_width = width;
-                 field_precision = pre
-               }
-            in
-            let c = s.[i] in
-            let i = succ i in
-            let fmt = String.sub s index (i - index) in
-               match c with
-                  'd' | 'i' -> print_int     buf i len s fmt info SignedDecimal
-                | 'u'       -> print_int     buf i len s fmt info UnsignedDecimal
-                | 'x'       -> print_int     buf i len s fmt info UnsignedHexLowercase
-                | 'X'       -> print_int     buf i len s fmt info UnsignedHexUppercase
-                | 'o'       -> print_int     buf i len s fmt info UnsignedOctal
-                | 's'       -> print_string  buf i len s fmt info
-                | 'c'       -> print_char    buf i len s fmt info
-                | 'f'       -> print_float   buf i len s fmt info FloatNormal
-                | 'e' | 'E' -> print_float   buf i len s fmt info FloatExp
-                | 'g' | 'G' -> print_float   buf i len s fmt info FloatBest
-                | 'b'       -> print_bool    buf i len s fmt info
-                | 'a'       -> print_user2   buf i len s fmt info
-                | 't'       -> print_user1   buf i len s fmt info
-                | '%'       -> print_percent buf i len s fmt info
-                | _         -> raise (Invalid_argument "parse_spec")
-      in
-         parse_flags [] (succ index)
-
-   (************************************************************************
-    * FORMAT CONTROL
-    ************************************************************************)
-
-   (*
-    * Parse the format string.
-    *)
-   and print_rformat buf index len s =
-      (*
-       * Look for some options in <options> format,
-       * separated by white space.
-       *)
-      let scratch = Buffer.create 19 in
-      let rec parse_options i cont =
-         if i = len then
-            cont i []
-         else
-            let c = s.[i] in
-               if c = '<' then
-                  parse_args [] (succ i) cont
-               else
-                  cont i []
-
-      and parse_args options i cont =
-         if i = len then
-            cont i (List.rev options)
-         else
-            let c = s.[i] in
-               match c with
-                  ' ' | '\t' | '\n' ->
-                     if Buffer.length scratch <> 0 then
-                        let s = Buffer.contents scratch in
-                           Buffer.clear scratch;
-                           parse_args (s :: options) (succ i) cont
-                     else
-                        parse_args options (succ i) cont
-                | '>' ->
-                     cont (succ i) (List.rev options)
-                | c ->
-                     Buffer.add_char scratch c;
-                     parse_args options (succ i) cont
-      in
-
-      (*
-       * Now we should have the spec.
-       *)
-      let parse_spec i =
-         if i = len then
-            print_dummy buf i len s
-         else
-            let c = s.[i] in
-            let i = succ i in
-               match c with
-                  '[' ->
-                     parse_options i (fun i options ->
-                           let box, indent =
-                              match options with
-                                 box :: indent :: _ ->
-                                    box, int_of_string indent
-                               | [box] ->
-                                    box, 0
-                               | [] ->
-                                    "b", 0
-                           in
-                           let _ =
-                              match box with
-                                 "h" -> Args.open_hbox buf
-                               | "v" -> Args.open_vbox buf indent
-                               | "hv" -> Args.open_hvbox buf indent
-                               | "hov" -> Args.open_hovbox buf indent
-                               | "b" -> Args.open_box buf indent
-                               | _ -> raise (Invalid_argument ("print_rformat: bogus box type \"" ^ String.escaped box ^ "\""))
-                           in
-                              print_loop buf i len s)
-                | ']' ->
-                     Args.close_box buf;
-                     print_loop buf i len s
-                | ',' ->
-                     Args.print_cut buf;
-                     print_loop buf i len s
-                | ' ' ->
-                     Args.print_space buf;
-                     print_loop buf i len s
-                | '\n' ->
-                     Args.force_newline buf;
-                     print_loop buf i len s
-                | ';' ->
-                     parse_options i (fun i options ->
-                           let nspaces, off =
-                              match options with
-                                 nspaces :: off :: _ -> int_of_string nspaces, int_of_string off
-                               | [nspaces] -> int_of_string nspaces, 0
-                               | [] -> 0, 0
-                           in
-                              Args.print_break buf nspaces off;
-                              print_loop buf i len s)
-                | '?' ->
-                     Args.print_flush buf;
-                     print_loop buf i len s
-                | '.' ->
-                     Args.print_newline buf;
-                     print_loop buf i len s
-                | c ->
-                     Args.print_char buf c;
-                     print_loop buf i len s
-        in
-           parse_spec (succ index)
-
-   (*
-    * The is the main printf function.
-    *)
-   and print_loop buf i len s =
-      if i = len then
-         Obj.magic (Args.exit buf)
-      else
-         match s.[i] with
-            '%' ->
-               print_format buf i len s
-          | '@' ->
-               print_rformat buf i len s
-          | c ->
-               Args.print_char buf c;
-               print_loop buf (succ i) len s
-
-   (*
-    * Outermost printf function.
-    *)
-   let fprintf buf s =
-      let s = string_of_format s in
-      print_loop buf 0 (String.length s) s
+   let fprintf o (Format (fmt,_)) = 
+      make_printf (fun acc -> print_acc o acc; Args.exit o) End_of_acc fmt
 end
 
 (*!
